aff73770b028dde24c446dea5b3fd74fc7d2a762
Milena <milena.engelke@ukr.de>
2022-04-28 15:54:27 +0200

    sced data set

---
+++ sced.R
@@ -0,0 +1,20 @@
+library(readr)
+library(tidyverse)
+
+
+# 1. load data frame sced1 ----
+
+sced <- readRDS(file = "sced1.RDS") 
+
+
+# 2. subset subjects for demographics 
+
+sub <- sced %>%
+  group_by(case_id) %>%
+  arrange(con_days) %>%
+  filter(row_number()==1) %>%
+  select(1:3,6:8,17:43) %>% 
+  arrange(case_id) %>% 
+  ungroup()
+
+
